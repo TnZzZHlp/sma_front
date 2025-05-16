2b92c4d4f48b0509abfa69246c2dccb3c3420f4d
TnZzZHlp <tnzzzhlp@outlook.com>
2025-05-16 11:27:24 +0800

    fix(Nav): 调整导航项的显示位置和间距

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -5,7 +5,7 @@ export default function Nav() {
     return (
         <div
             className={`absolute left-1/2 transform -translate-x-1/2 flex items-center justify-center rounded-lg bg-white shadow-xl/25  rounded-xl
- ${showNav ? "top-3" : "-top-12"} transition-all duration-500 ease-in-out`}
+ ${showNav ? "top-3" : "-top-10"} transition-all duration-500 ease-in-out`}
             onMouseEnter={() => setShowNav(true)}
             onMouseLeave={() => setShowNav(false)}
         >
@@ -24,7 +24,7 @@ function NavItem({ text, onClick }: { text: string; onClick?: () => void }) {
 
     return (
         <div
-            className={`m-4 cursor-pointer px-4 py-2 rounded-lg transition-all duration-500 ease-in-out  ${hoverStyle}`}
+            className={`m-2 cursor-pointer px-4 py-2 rounded-lg transition-all duration-500 ease-in-out  ${hoverStyle}`}
             onClick={onClick}
             onMouseEnter={() => setHover(true)}
             onMouseLeave={() => setHover(false)}
