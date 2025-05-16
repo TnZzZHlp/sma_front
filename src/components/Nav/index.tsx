2e9242b2a044c6078af678597291f95f35d50a20
TnZzZHlp <tnzzzhlp@outlook.com>
2025-05-16 11:45:31 +0800

    feat(Nav): 添加导航项的触摸事件处理和自动隐藏功能

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -5,14 +5,35 @@ export default function Nav() {
     return (
         <div
             className={`absolute left-1/2 transform -translate-x-1/2 flex items-center justify-center rounded-lg bg-white shadow-xl/25  rounded-xl
- ${showNav ? "top-3" : "-top-10"} transition-all duration-500 ease-in-out`}
-            onMouseEnter={() => setShowNav(true)}
-            onMouseLeave={() => setShowNav(false)}
+ ${showNav ? "top-3" : "-top-14 pb-4"} transition-all duration-500 ease-in-out`}
+            onMouseEnter={() => {
+                setShowNav(true);
+                setTimeout(() => {
+                    setShowNav(false);
+                }, 3000);
+            }}
+            onTouchStart={() => {
+                setShowNav(true);
+                setTimeout(() => {
+                    setShowNav(false);
+                }, 3000);
+            }}
         >
-            <NavItem text="解题" onClick={() => (window.location.href = "/")} />
+            <NavItem
+                text="解题"
+                onClick={() => {
+                    if (window.location.href !== "/" || showNav) {
+                        window.location.href = "/";
+                    }
+                }}
+            />
             <NavItem
                 text="检查"
-                onClick={() => (window.location.href = "/check")}
+                onClick={() => {
+                    if (window.location.href !== "/check" || showNav) {
+                        window.location.href = "/check";
+                    }
+                }}
             />
         </div>
     );
@@ -28,6 +49,8 @@ function NavItem({ text, onClick }: { text: string; onClick?: () => void }) {
             onClick={onClick}
             onMouseEnter={() => setHover(true)}
             onMouseLeave={() => setHover(false)}
+            onTouchStart={() => setHover(true)}
+            onTouchEnd={() => setHover(false)}
         >
             {text}
         </div>
